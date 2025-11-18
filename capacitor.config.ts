f3fc1fd042f81784d93187a9b523d570d8775712
Алексей Воробьёв <Schizooo95@gmail.com>
2025-11-18 13:06:39 +0500

    feat: add Android app with Capacitor - hybrid mode with camera access

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'kz.anonimka.app',
+  appName: 'Anonimka',
+  webDir: 'out'
+};
+
+export default config;
