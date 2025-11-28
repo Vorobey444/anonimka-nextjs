cf1ece35c237701f4df0b18e14cc9c0134b6eeb5
Алексей Воробьёв <Schizooo95@gmail.com>
2025-11-28 23:39:39 +0500

    fix: disable cache for /webapp/* files to prevent outdated JS

--- next.config.js
+++ next.config.js
@@ -20,6 +20,23 @@ const nextConfig = {
             value: 'Content-Type, Authorization'
           }
         ]
+      },
+      {
+        source: '/webapp/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate'
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache'
+          },
+          {
+            key: 'Expires',
+            value: '0'
+          }
+        ]
       }
     ]
   },
