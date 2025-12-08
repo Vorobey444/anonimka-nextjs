919cccbbe1b87f1704becaa6fde52a9353e79834
Алексей Воробьёв <Schizooo95@gmail.com>
2025-12-08 12:19:21 +0500

    chore: remove all legacy files from git

--- remove_ads_user_token_unique.sql
+++
@@ -1,7 +0,0 @@
--- Удаляем UNIQUE constraint на ads.user_token
--- Это позволит пользователям создавать несколько активных анкет
-
-ALTER TABLE ads DROP CONSTRAINT IF EXISTS ads_user_token_key;
-
--- Проверка: должно быть OK
-SELECT 'Constraint removed successfully' AS status;
